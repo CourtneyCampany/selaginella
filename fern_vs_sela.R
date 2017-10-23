d1365650710430481d925f5219f44b1c473a3e56
Court Campany <ccampany@colgate.edu>
2017-10-23 16:23:03 -0400

    started pca, sela vs fern

--- fern_vs_sela.R
+++ fern_vs_sela.R
@@ -1,17 +1,22 @@
 source("master_scripts/plot_objects.R")
 library(doBy)
+
 ##sela vs ferns
 
 sumfun <- function(x, ...){
   c(m=mean(x, ...),l=length(x))
 }
 
-
 se <- function(x) sd(x)/sqrt(length(na.omit(x)))
 
 
-
 gas_exchange <- read.csv("raw_data/gas_exchange.csv")
+#order species for plotting
+gas_exchange$species <- factor(gas_exchange$species, levels=c("Sel_anc","Sel_art","Sel_ate",
+                                                        "Sel_eur","Sel_oax","Sel_swim","Sel_umb",
+                                                        "Bul_port","Cyc_semi","Dip_stri","Thy_cur"))
+
+
 photo_family <-summaryBy(. ~ family, data=gas_exchange, FUN=c(mean,se))
 
 chem <- read.csv("raw_data/leaf_chemistry.csv")
@@ -30,3 +35,17 @@ chem_agg <- ddply(chem2, "family", summarise,
                se   = sd / sqrt(N)
 )
 
+
+
+# boxplots ----------------------------------------------------------------
+par(mar=c(5,4,1,1), mgp=c(2.5,1,0))
+boxplot(photo ~ species, data=gas_exchange, ylab=photolab, xaxt='n', outline=FALSE, at=c(1:7, 9:12))
+text(cex=1, x=c(1:7, 9:12), y=0, labels=unique(gas_exchange$species), xpd=TRUE, srt=45)
+axis(1, at=c(1:7, 9:12), labels=FALSE)
+
+boxplot(cond ~ species, data=gas_exchange, ylab=condlab, xaxt='n', outline=FALSE,at=c(1:7, 9:12))
+text(cex=1, x=c(1:7, 9:12), y=0, labels=unique(gas_exchange$species), xpd=TRUE, srt=45)
+axis(1, at=c(1:7, 9:12), labels=FALSE)
+
+
+#make a means dataframe and a series of boxplots with raw data
---
+++ sela_pca.R
@@ -0,0 +1,55 @@
+#pca with selaginella
+
+sela <- read.csv("raw_data/sela_raw.csv")
+sela2 <- sela[,-c(1:2)]
+
+library(vegan)
+
+##look at variances
+var(sela$cond)
+var(sela$lma)
+#variances among variables are orders of magnitude different so we will rescale
+
+sela_dca <- decorana(na.omit(sela2))
+summary(sela_dca, display = 'none') #since axis length is less than 3 so we use PCA
+
+#principle compoent analysis with scales variances
+sela_rda<- rda(na.omit(sela2),scale=T)
+plot(sela_rda)
+summary(sela_rda)
+
+#nicer plot
+
+library(scales)
+sites <- scores(sela_rda, display='sites')
+spp <- scores(sela_rda, display='species')
+
+
+library(RColorBrewer)
+cols <- brewer.pal(7, "Dark2")
+cols2 <- c(rep(cols[1],5), rep(cols[2],5),rep(cols[3],5),rep(cols[4],5),rep(cols[5],5),
+           rep(cols[6],5),rep(cols[7],5))
+
+
+plot(sites,ylab="PC 2 (26.5 %)", xlab="PC 1 (43.4 %)",
+     cex=1, col=alpha(cols2, .8), pch=16,xlim=c(-2.5, 2.5), ylim=c(-1.5, 1.5))
+
+text(spp,labels=rownames(scores(sela_rda, display='species')),cex=0.8, col="black")
+abline(v=0, lty='dashed')
+abline(h=0, lty='dashed')
+
+legend("topright", legend= unique(sela$species),col=cols, pch=16, inset=0.01,bty='n', cex=.8 )
+
+#biplot(sela_rda)
+
+# use prcomp --------------------------------------------------------------
+
+sela_prc <- prcomp(na.omit(sela2),scale=T)
+summary(sela_prc)
+sites2 <- scores(sela_prc, display='sites')
+spp2 <- scores(sela_prc, display='species')
+biplot(sela_prc, pch=16, col=c(cols, "blue"))
+
+plot(sites2,ylab="PC 2 (26.5 %)", xlab="PC 1 (43.4 %)",
+     cex=1, col=alpha(cols2, .8), pch=16,xlim=c(-2.5, 2.5), ylim=c(-1.5, 1.5))
+text(spp,labels=rownames(scores(sela_rda, display='species')),cex=0.8, col="black")
