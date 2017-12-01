606f1721922f7dd497be27553f8267aed68d2a43
Court Campany <ccampany@colgate.edu>
2017-12-01 15:25:58 -0500

    started stats

--- master_scripts/sela_fern_nitro.R
+++ master_scripts/sela_fern_nitro.R
@@ -14,38 +14,39 @@ sela_chem_agg <- doBy::summaryBy(. ~ family + species, data=sela_chem, FUN=c(mea
 sela_stom_agg <-doBy::summaryBy(. ~ family + species, data=sela_stom, FUN=c(mean, se))
 
 #merge selaginella data sets
-sela_agg <- merge(sela_chem_agg[,c(1:2, 5, 12)], sela_stom_agg[,c(1:2, 4,9)])
+sela_agg <- merge(sela_chem_agg[,c(1:2, 5, 12)], sela_stom_agg[,c(1:2, 6,12)])
 
 
 # plotting ----------------------------------------------------------------
 terr <- fern_cr[fern_cr$habitat=="terrestrial" | fern_cr$habitat == "hemi-epiphyte",]
 
-windows(7,7)
+png(filename = "output/lma_nitro.png", width = 11, height = 8.5, units = "in", res= 400)
+#windows(7,7)
 par(mar=c(5,5,2,2), las=1)
-plot(percN.mean ~ lma_µgpermm2.mean, data=sela_agg, xlim=c(0, 55), ylim=c(0, 6.5), 
+plot(percN.mean ~ lma_gpercm2.mean, data=sela_agg, xlim=c(0, 55), ylim=c(0, 6.5), 
     col=famcols[family], xlab=lmalab, ylab="Leaf Nitrogen (%)", type='n')
-with(sela_agg, arrows(lma_µgpermm2.mean, percN.mean, lma_µgpermm2.mean, 
+with(sela_agg, arrows(lma_gpercm2.mean, percN.mean, lma_gpercm2.mean, 
                       percN.mean+percN.se,col=famcols[family],
                       angle=90, length=0.03, cex=1.5))
-with(sela_agg, arrows(lma_µgpermm2.mean, percN.mean, lma_µgpermm2.mean, 
+with(sela_agg, arrows(lma_gpercm2.mean, percN.mean, lma_gpercm2.mean, 
                       percN.mean-percN.se,col=famcols[family],
                       angle=90, length=0.03, cex=1.5))
-points(percN.mean ~ lma_µgpermm2.mean, data=sela_agg,pch=16,
+points(percN.mean ~ lma_gpercm2.mean, data=sela_agg,pch=16,
         col=famcols[family], cex=1.5)
 
-with(sela_agg, arrows(lma_µgpermm2.mean, percN.mean, lma_µgpermm2.mean+lma_µgpermm2.se, 
+with(sela_agg, arrows(lma_gpercm2.mean, percN.mean, lma_gpercm2.mean+lma_gpercm2.se, 
                       percN.mean,col=famcols[family],
                       angle=90, length=0.03, cex=1.5))
-with(sela_agg, arrows(lma_µgpermm2.mean, percN.mean, lma_µgpermm2.mean-lma_µgpermm2.se, 
+with(sela_agg, arrows(lma_gpercm2.mean, percN.mean, lma_gpercm2.mean-lma_gpercm2.se, 
                       percN.mean,col=famcols[family],
                       angle=90, length=0.03, cex=1.5))
 
 points((percN_mgperg)/10 ~ lma_gpercm, data=terr,pch=21, bg=famcols[1], cex=1.5)
 
-legend("topleft", legend=c("Ferns", "Selaginella", "Ferns-survey"), 
-       col=c(familycols[1],familycols[2],"black"),pt.bg=familycols[1],
-       pch=c(16, 16, 21), bty='n', inset=.01, cex=1)
+legend("topleft", legend=c("Selaginella","Ferns", "Ferns-Watkins et al. 2007"), 
+       col=c(familycols[2],familycols[1],"black"),pt.bg=familycols[1],
+       pch=c(16, 16, 21), bty='n', inset=.01, cex=.9)
 
-dev.copy2pdf(file= "output/fern_sela_survey.pdf")
+# dev.copy2pdf(file= "output/fern_sela_survey.pdf")
 dev.off()
 
---
+++ stats/sela_stats.R
@@ -0,0 +1,166 @@
+#selaginella stats
+library(visreg)
+library(multcomp)
+library(car)
+
+#pca with selaginella
+sela <- read.csv("raw_data/sela_raw.csv")
+sela$species <- factor(sela$species, 
+              levels=c("Sel_anc","Sel_art","Sel_ate","Sel_eur","Sel_oax","Sel_swim","Sel_umb"))
+
+#run annova for each parameter 
+
+
+# photosynthesis ----------------------------------------------------------
+asat_mod <- lm(asat ~ species, data=sela)
+
+visreg(asat_mod)
+residualPlot(asat_mod)
+qqPlot(asat_mod)
+
+summary(asat_mod)
+anova(asat_mod)
+
+tukey_asat <- glht(asat_mod, linfct = mcp(species = "Tukey"))
+asat_siglets <-cld(tukey_asat)
+asat_siglets2 <- asat_siglets$mcletters$Letters
+
+# conductance ----------------------------------------------------------
+gs_mod <- lm(gs
+             ~ species, data=sela)
+
+visreg(gs_mod)
+residualPlot(gs_mod)
+qqPlot(gs_mod)
+
+summary(gs_mod)
+anova(gs_mod)
+
+tukey_gs <- glht(gs_mod, linfct = mcp(species = "Tukey"))
+gs_siglets <-cld(tukey_gs)
+gs_siglets2 <- gs_siglets$mcletters$Letters
+
+# chlorophyll ----------------------------------------------------------
+chl_mod <- lm(chlorophyll ~ species, data=sela)
+
+visreg(chl_mod)
+residualPlot(chl_mod)
+qqPlot(chl_mod)
+
+summary(chl_mod)
+anova(chl_mod)
+
+tukey_chl <- glht(chl_mod, linfct = mcp(species = "Tukey"))
+chl_siglets <-cld(tukey_chl)
+chl_siglets2 <- chl_siglets$mcletters$Letters
+
+# LMA ----------------------------------------------------------
+LMA_mod <- lm(LMA ~ species, data=sela)
+
+visreg(LMA_mod)
+residualPlot(LMA_mod)
+qqPlot(LMA_mod)
+
+summary(LMA_mod)
+anova(LMA_mod)
+
+tukey_lma <- glht(LMA_mod, linfct = mcp(species = "Tukey"))
+lma_siglets <-cld(tukey_lma)
+lma_siglets2 <- lma_siglets$mcletters$Letters
+
+# N ----------------------------------------------------------
+n_mod <- lm(N ~ species, data=sela)
+
+visreg(n_mod)
+residualPlot(n_mod)
+qqPlot(n_mod)
+
+summary(n_mod)
+anova(n_mod)
+
+tukey_n <- glht(n_mod, linfct = mcp(species = "Tukey"))
+n_siglets <-cld(tukey_n)
+n_siglets2 <- n_siglets$mcletters$Letters
+
+# P ----------------------------------------------------------
+p_mod <- lm(P ~ species, data=sela)
+
+visreg(p_mod)
+residualPlot(p_mod)
+qqPlot(p_mod)
+
+summary(p_mod)
+anova(p_mod)
+
+tukey_p <- glht(p_mod, linfct = mcp(species = "Tukey"))
+p_siglets <-cld(tukey_lma)
+p_siglets2 <- p_siglets$mcletters$Letters
+
+# sto_dens ----------------------------------------------------------
+sd_mod <- lm(sto_dens ~ species, data=sela)
+
+visreg(sd_mod)
+residualPlot(sd_mod)
+qqPlot(sd_mod)
+
+summary(sd_mod)
+anova(sd_mod)
+
+tukey_sd <- glht(sd_mod, linfct = mcp(species = "Tukey"))
+sd_siglets <-cld(tukey_sd)
+sd_siglets2 <- sd_siglets$mcletters$Letters
+
+# sto_length ----------------------------------------------------------
+sl_mod <- lm(sto_length ~ species, data=sela)
+
+visreg(sl_mod)
+residualPlot(sl_mod)
+qqPlot(sl_mod)
+
+summary(sl_mod)
+anova(sl_mod)
+
+tukey_sl <- glht(sl_mod, linfct = mcp(species = "Tukey"))
+sl_siglets <-cld(tukey_sl)
+sl_siglets2 <- sl_siglets$mcletters$Letters
+
+
+# ITE ----------------------------------------------------------
+ITE_mod <- lm(ITE ~ species, data=sela)
+summary(ITE_mod)
+visreg(ITE_mod)
+
+anova(ITE_mod)
+
+tukey_ite <- glht(ITE_mod, linfct = mcp(species = "Tukey"))
+ite_siglets <-cld(tukey_ite)
+ite_siglets2 <- ite_siglets$mcletters$Letters
+
+# C.N ----------------------------------------------------------
+cn_mod <- lm(C.N ~ species, data=sela)
+
+visreg(cn_mod)
+residualPlot(cn_mod)
+qqPlot(cn_mod)
+
+summary(cn_mod)
+anova(cn_mod)
+
+tukey_cn <- glht(cn_mod, linfct = mcp(species = "Tukey"))
+cn_siglets <-cld(tukey_cn)
+cn_siglets2 <- cn_siglets$mcletters$Letters
+
+# lcp ----------------------------------------------------------
+lcp_mod <- lm(lcp ~ species, data=sela)
+summary(lcp_mod)
+visreg(lcp_mod)
+
+anova(lcp_mod)
+
+tukey_lcp<- glht(lcp_mod, linfct = mcp(species = "Tukey"))
+lcp_siglets <-cld(tukey_lcp)
+lcp_siglets2 <- lcp_siglets$mcletters$Letters
+
+
+
+
